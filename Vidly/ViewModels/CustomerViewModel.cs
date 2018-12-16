f98d0c7b9a0827933f6d6321f6c988aaad7a3904
Damian Bouch <dbouch4009@gmail.com>
2018-12-15 22:05:04 -0500

    part one done

---
+++ Vidly/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
